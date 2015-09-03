00a725b8fa6923ab75a9325d859d8c1ded109faa
luzhlon <luzhlon@126.com>
2015-09-03 12:46:40 +0800

    initail test

---
+++ linux-build/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/john/cmake_proj/game/Classes/AppDelegate.cpp" "/home/john/cmake_proj/game/linux-build/CMakeFiles/Game.dir/Classes/AppDelegate.cpp.o"
+  "/home/john/cmake_proj/game/Classes/GameScene.cpp" "/home/john/cmake_proj/game/linux-build/CMakeFiles/Game.dir/Classes/GameScene.cpp.o"
+  "/home/john/cmake_proj/game/Classes/SceneLayer.cpp" "/home/john/cmake_proj/game/linux-build/CMakeFiles/Game.dir/Classes/SceneLayer.cpp.o"
+  "/home/john/cmake_proj/game/Classes/Soldier.cpp" "/home/john/cmake_proj/game/linux-build/CMakeFiles/Game.dir/Classes/Soldier.cpp.o"
+  "/home/john/cmake_proj/game/proj.linux/main.cpp" "/home/john/cmake_proj/game/linux-build/CMakeFiles/Game.dir/proj.linux/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "LINUX"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/john/cmake_proj/game/linux-build/cocos2d/cocos/CMakeFiles/cocos2d.dir/DependInfo.cmake"
+  "/home/john/cmake_proj/game/linux-build/cocos2d/external/unzip/CMakeFiles/unzip.dir/DependInfo.cmake"
+  "/home/john/cmake_proj/game/linux-build/cocos2d/external/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake"
+  "/home/john/cmake_proj/game/linux-build/cocos2d/external/xxhash/CMakeFiles/xxhash.dir/DependInfo.cmake"
+  "/home/john/cmake_proj/game/linux-build/cocos2d/external/bullet/CMakeFiles/bullet.dir/DependInfo.cmake"
+  "/home/john/cmake_proj/game/linux-build/cocos2d/external/recast/CMakeFiles/recast.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/GLFW"
+  "/usr/include/GLFW"
+  "../cocos2d"
+  "../cocos2d/cocos"
+  "../cocos2d/external"
+  "../cocos2d/cocos/platform"
+  "../cocos2d/cocos/audio/include"
+  "../cocos2d/cocos/editor-support"
+  "../Classes"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
